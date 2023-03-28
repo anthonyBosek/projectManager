c0f79cb4f62f3c4dee4463d0c4917880fb9f130c
Anthony Bosek <anthony.bosek@gmail.com>
2023-03-28 07:10:20 -0700

    update & delete project components w/ mutations

--- client/src/mutations/projectMutations.js
+++ client/src/mutations/projectMutations.js
@@ -1,5 +1,6 @@
 import { gql } from "@apollo/client";
 
+// Add a new project
 const ADD_PROJECT = gql`
   mutation AddProject(
     $name: String!
@@ -27,4 +28,41 @@ const ADD_PROJECT = gql`
   }
 `;
 
-export { ADD_PROJECT };
+// Delete a project
+const DELETE_PROJECT = gql`
+  mutation DeleteProject($id: ID!) {
+    deleteProject(id: $id) {
+      id
+    }
+  }
+`;
+
+// Update a project
+const UPDATE_PROJECT = gql`
+  mutation UpdateProject(
+    $id: ID!
+    $name: String!
+    $description: String!
+    $status: ProjectStatusUpdate!
+  ) {
+    updateProject(
+      id: $id
+      name: $name
+      description: $description
+      status: $status
+    ) {
+      id
+      name
+      description
+      status
+      client {
+        id
+        name
+        email
+        phone
+      }
+    }
+  }
+`;
+
+export { ADD_PROJECT, DELETE_PROJECT, UPDATE_PROJECT };
